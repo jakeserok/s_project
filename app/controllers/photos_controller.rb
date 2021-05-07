a251da2ddabce4608eb5f994bf15fc03df094e1c
jakeserok <jakeserok@gmail.com>
2021-05-06 22:04:54 -0400

    Added ability to create and view photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,32 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to @photo, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @photo }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:title, :image)
+    end
 end
